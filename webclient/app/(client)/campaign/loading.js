d7ae884156fd85bae0b2e60fe189358ba0aa2eb7
khmalz <akmalrma1710@gmail.com>
2025-02-17 17:33:19 +0700

    add not found and loading on campaign fund page

---
+++ webclient/app/(client)/campaign/loading.js
@@ -0,0 +1,7 @@
+export default function Loading() {
+   return (
+      <div className="w-full h-screen flex flex-col items-center justify-center">
+         <h2 className="text-2xl font-bold">Loading...</h2>
+      </div>
+   );
+}
