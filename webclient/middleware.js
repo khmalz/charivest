dac855ec08d113ef9bfad7a7977d90de9b249920
khmalz <akmalrma1710@gmail.com>
2025-02-17 22:03:34 +0700

    add wip cookies and middleware using crypto to encrypt

---
+++ webclient/middleware.js
@@ -0,0 +1,30 @@
+import { NextResponse } from "next/server";
+import { verifySession } from "./app/lib/dal";
+
+const protectedRoutes = ["/dashboard", "/campaign"];
+const publicRoutes = ["/"];
+
+export default async function middleware(req) {
+   const path = req.nextUrl.pathname;
+   console.log(path);
+
+   const isProtectedRoute = protectedRoutes.some(route => path.startsWith(route));
+   const isPublicRoute = publicRoutes.includes(path);
+
+   const { isAuth, _ } = await verifySession();
+
+   console.log(isAuth);
+   if (isProtectedRoute && !isAuth) {
+      return NextResponse.redirect(new URL("/", req.nextUrl));
+   }
+
+   if (isPublicRoute && isAuth && !req.nextUrl.pathname.startsWith("/dashboard")) {
+      return NextResponse.redirect(new URL("/dashboard", req.nextUrl));
+   }
+
+   return NextResponse.next();
+}
+
+export const config = {
+   matcher: ["/((?!api|_next/static|_next/image|.*\\.png$).*)"],
+};
