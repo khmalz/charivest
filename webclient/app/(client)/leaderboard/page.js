c599166dbca397a89b0175a9655b97b30b4bddd6
khmalz <akmalrma1710@gmail.com>
2025-02-20 11:40:50 +0700

    get list leaderboard from contracts

--- webclient/app/(client)/leaderboard/page.js
+++ webclient/app/(client)/leaderboard/page.js
@@ -1,80 +1,12 @@
 import LeaderboardTable from "@/components/ui/LeaderboardTable";
 
 export default function Leaderboard() {
-   const leaderboards = [
-      {
-         id: 1,
-         name: "John",
-         total_fund: 10000,
-      },
-      {
-         id: 2,
-         name: "Marry",
-         total_fund: 2000,
-      },
-      {
-         id: 3,
-         name: "Carol",
-         total_fund: 1000,
-      },
-      {
-         id: 4,
-         name: "Bob",
-         total_fund: 900,
-      },
-      {
-         id: 5,
-         name: "Alice",
-         total_fund: 800,
-      },
-      {
-         id: 6,
-         name: "Andrew",
-         total_fund: 9000,
-      },
-      {
-         id: 7,
-         name: "Kevin",
-         total_fund: 2000,
-      },
-      {
-         id: 8,
-         name: "Tom",
-         total_fund: 1000,
-      },
-      {
-         id: 9,
-         name: "Bob",
-         total_fund: 900,
-      },
-      {
-         id: 10,
-         name: "Alice",
-         total_fund: 800,
-      },
-      {
-         id: 11,
-         name: "Andrew",
-         total_fund: 9000,
-      },
-      {
-         id: 12,
-         name: "Kevin",
-         total_fund: 2000,
-      },
-      {
-         id: 13,
-         name: "Tom",
-         total_fund: 1000,
-      },
-   ];
-
    return (
       <section id="leaderbord" className="mt-5">
          <div>
             <h2 className="text-2xl font-bold dark:text-white text-slate-900 sm:text-4xl text-center">Leaderboard</h2>
 
-            <LeaderboardTable data={leaderboards} />
+            <LeaderboardTable />
          </div>
       </section>
    );
